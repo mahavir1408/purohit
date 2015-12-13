53e05cbb57cb0e4508e0c5a714d21b5f9069c8e1
Mahavir Munot <mahavir1408@gmail.com>
2015-12-13 13:03:46 +0530

    Populate the data on EditMonthlyBill page

--- js/EditMonthlyBill.js
+++ js/EditMonthlyBill.js
@@ -1,84 +1,59 @@
 // JavaScript Document
 $(document).ready(function () {
-			$(".bgoverlay").hide();
-			$("#updateMonthlyBill").validationEngine({scroll: true, showArrow: true });
+	$(".bgoverlay").hide();
+	$("#updateMonthlyBill").validationEngine({scroll: true, showArrow: true });
 			
-			var qs = $.url().param('bid');
-			$('#idHfBillNum').val(qs);
+	var qs = $.url().param('bid');
+	$('#idHfBillNum').val(qs);
             
-			$.ajax({
-                type:"POST",
-                url: "Get_EditMonthlyBill.php?bid="+qs,
-                datatype: "json",
-				data: "{}",
-                contentType: "application/json; charset=utf-8",
-                success: function(data,textStatus,xhr) {
-                   data = JSON.parse(xhr.responseText);
-                   var i = 0;
-				   
-				   var snamesplit = data[i].sname;
-				   var snamesplit = snamesplit.split(" - ");
-				   
-                   $('#idTxPaybyCid').val(data[i].payby);	
-				   $('#idTxPayby').val(data[i].paybyname);
-				   $('#idTxShortName').val(snamesplit[0]);
-				   $('#idTxLocation').val(snamesplit[1]);
-				   $('#idTxState').val(data[i].state);
-				   $('#idTxCity').val(data[i].city);
-				   $('#idTxPaybyPin').val(data[i].pin);
-				   $('#idTxPaybyAl1').val(data[i].al1);
-				   $('#idTxPaybyAl2').val(data[i].al2);
-				   $('#idTxPaybyCell').val(data[i].contact);
-				   $('#idTxPaybyEmail').val(data[i].emailid);
-				   
-				   $('#idTxBillDt').val(data[i].date);	
-				   $('#idTxFromDt').val(data[i].fromdate);	
-				   $('#idTxToDt').val(data[i].todate);	  
- 
-//				   var setax = tot * setaxrate / 100;
-//					$('#txIdServcieTax').val(setax);
-//			
-//					var educhess = setax * educhessrate / 100;
-//					$('#txIdEduChess').val(educhess);
-//			
-//					var heduchess = setax * heduchessrate / 100;
-//					$('#txIdHighEduChess').val(heduchess);
-//					var labourcharge = $('#txIdLaboutCharges').val();
-//					var waitingcharge = $('#txIdWaitingCharges').val();	
-//					var othercharge = $('#txIdOtherCharges').val();	
-//			
-//					var Pregt = ( 1*tot + 1*setax + 1*educhess + 1*heduchess + 1*labourcharge + 1*waitingcharge + 1*othercharge );
-//				    $('#txIdGT').val(data[i].gt);
-//				   
-//				   
-//				   $('#txIdTot').val(data[i].subtot);
-//				   $('#txIdServcieTax').val(data[i].setax);
-//				   $('#txIdEduChess').val(data[i].educhess);
-//				   $('#txIdHighEduChess').val(data[i].higheduchess);
-//				   $('#idTxServcieTaxRate').val(data[i].setaxrate);
-//				   $('#idTxEduChessRate').val(data[i].educhessrate);
-//				   $('#idTxHighEduChessRate').val(data[i].higheduchessrate);
-				   
-				   $('#txIdLaboutCharges').val(data[i].laborcharges);
-				   $('#txIdWaitingCharges').val(data[i].waitingcharges);
-				   $('#txIdOtherCharges').val(data[i].othercharges);
-				  
-				   $('#idSePayment').val(data[i].payment);
-				   $('#txIdPayDate').val(data[i].paydate);
-				   $('#idTxBankName').val(data[i].bankname);
-				   $('#idTxChequeNum').val(data[i].chequenum);
-				   
-				   var payby = data[i].payby
-				   var fromDt = data[i].fromdate;
-				   var toDt = data[i].todate;
+	$.ajax({
+        type:"POST",
+        url: "Get_EditMonthlyBill.php?bid="+qs,
+        datatype: "json",
+		data: "{}",
+        contentType: "application/json; charset=utf-8",
+        success: function(data,textStatus,xhr) {
+			data = JSON.parse(xhr.responseText);
+			var i = 0;
+
+			var snamesplit = data[i].sname;
+			var snamesplit = snamesplit.split(" - ");
+
+			$('#idTxPaybyCid').val(data[i].payby);	
+			$('#idTxPayby').val(data[i].paybyname);
+			$('#idTxShortName').val(snamesplit[0]);
+			$('#idTxLocation').val(snamesplit[1]);
+			$('#idTxState').val(data[i].state);
+			$('#idTxCity').val(data[i].city);
+			$('#idTxPaybyPin').val(data[i].pin);
+			$('#idTxPaybyAl1').val(data[i].al1);
+			$('#idTxPaybyAl2').val(data[i].al2);
+			$('#idTxPaybyCell').val(data[i].contact);
+			$('#idTxPaybyEmail').val(data[i].emailid);
+
+			$('#idTxBillDt').val(data[i].date);	
+			$('#idTxFromDt').val(data[i].fromdate);	
+			$('#idTxToDt').val(data[i].todate);	  
+
+
+			$('#txIdLaboutCharges').val(data[i].laborcharges);
+			$('#txIdWaitingCharges').val(data[i].waitingcharges);
+			$('#txIdOtherCharges').val(data[i].othercharges);
+
+			$('#idSePayment').val(data[i].payment);
+			$('#txIdPayDate').val(data[i].paydate);
+			$('#idTxBankName').val(data[i].bankname);
+			$('#idTxChequeNum').val(data[i].chequenum);
+
+			var payby = data[i].payby
+			var fromDt = data[i].fromdate;
+			var toDt = data[i].todate;
 			
 			$.ajax({
                 type:"POST",
                 url: "Get_EditMonthlyBilll_DocketList.php?bid="+qs+"&from="+fromDt+"&to="+toDt+"&pay="+payby,
                 datatype: "json",
-				data: "{}",
-				//beforeSend: "",
-				//timeout: 5000,  
+				data: "{}", 
 				beforeSend : function(){
 					$(".bgoverlay").show();
 				},
@@ -86,46 +61,36 @@ $(document).ready(function () {
                 success: function(data,textStatus,xhr) {
                 	data = JSON.parse(xhr.responseText);  
 					var count = $('#idHfDocketCount').val();
-					$('#idTbDocketList').find("tr:gt(0)").remove();
-					
-					if ( data != "" && data != null) {
-					
+					$('#idTbDocketList').find("tr:gt(0)").remove();					
+					if ( data != "" && data != null) {					
 						for ( var i = 0, len = data.length ; i < len; i++ ) {
-						count = 1 * count + 1 * 1;
-						$('#idHfDocketCount').val(count);
-						
-						$('#idTbDocketList').append( '<tr id="idTrCkSelect'+count+'">' +
-						'<td><input type="checkbox" name="ckSelect'+count+'" id="idCkSelect'+count+'" value="'+data[i].did+'" style="margin:0 !important; display: block; text-align:left; padding:0px; width:50px; " dir="ltr" class="validate[groupRequired[payments]]" onclick="caltot()"  />  <input type="hidden" name="hfSelect'+count+'" id="idHfSelect'+count+'" value="'+data[i].did+'" /> </td>' +							
-						'<td>'+data[i].idid+'</td>' +
-						'<td>'+data[i].manualdocketnum+'</td>' +
-						'<td>'+data[i].date+'</td>' +
-						'<td>'+data[i].Paybyname+'</td>' +
-						'<td id="idTdTot'+count+'">'+data[i].tot+'</td>' +
-						'</tr>' );
-						if (data[i].billed == 1) {
-							$("#idCkSelect"+count).prop('checked', true);
+							count = 1 * count + 1 * 1;
+							$('#idHfDocketCount').val(count);
+							
+							$('#idTbDocketList').append( '<tr id="idTrCkSelect'+count+'">' +
+							'<td><input type="checkbox" name="ckSelect'+count+'" id="idCkSelect'+count+'" value="'+data[i].did+'" style="margin:0 !important; display: block; text-align:left; padding:0px; width:50px; " dir="ltr" class="validate[groupRequired[payments]]" onclick="caltot()"  />  <input type="hidden" name="hfSelect'+count+'" id="idHfSelect'+count+'" value="'+data[i].did+'" /> </td>' +							
+							'<td>'+data[i].idid+'</td>' +
+							'<td>'+data[i].manualdocketnum+'</td>' +
+							'<td>'+data[i].date+'</td>' +
+							'<td>'+data[i].Paybyname+'</td>' +
+							'<td id="idTdTot'+count+'">'+data[i].tot+'</td>' +
+							'</tr>' );
+							if (data[i].billed == 1) {
+								$("#idCkSelect"+count).prop('checked', true);
+							} else {
+								$("#idCkSelect"+count).prop('checked',false );
+							}					
 						}
-						else {
-							$("#idCkSelect"+count).prop('checked',false );
-						}
-					
-						}
-						
-					}
-					else {
+						caltot();						
+					} else {
 						$("#nodoc").show();
 						$(".bgoverlay").hide();
 						$("#nodoc").html("No Docket Found.").delay(2000).fadeOut(400);
 						
-					}
-					
-					$(".bgoverlay").hide();
-					
+					}					
+					$(".bgoverlay").hide();					
 				}
             }); // End Docket List Ajax
-			
-	 	//$(".bgoverlay").hide();
-		
 		}		
 	}); // End Ajax	
 	
@@ -137,14 +102,10 @@ $(document).ready(function () {
 		var fromDt = $('#idTxFromDt').val();
 		var toDt = $('#idTxToDt').val();
 		var payby = $('#idTxPaybyCid').val();
-		
-		//alert(frmDt);
-		
 		if ( fromDt != '' && toDt != '' && payby != '' && payby != '0' ) {
 			alert("sam");
 			$.ajax({
 				type:"POST",
-				//url: "Get_MonthlyBill_DocketList.php?from="+frmDt+"&to="+toDT+"&payby="+paybyId,
 				url: "Get_EditMonthlyBilll_DocketList.php?bid="+qs+"&from="+fromDt+"&to="+toDt+"&pay="+payby,
 				datatype: "json",
 				data: "{}",
@@ -175,14 +136,12 @@ $(document).ready(function () {
 						'</tr>' );
 						if (data[i].billed == 1) {
 							$("#idCkSelect"+count).prop('checked', true);
-						}
-						else {
+						} else {
 							$("#idCkSelect"+count).prop('checked',false );
-						}
-						
+						}						
 					} // End For
-				}
-				else {
+					caltot();
+				} else {
 					$("#nodoc").show();
 					$(".bgoverlay").hide();
 					$("#nodoc").html("No Docket Found.").delay(2000).fadeOut(400);
